5cdef9f52c39f30c16e392252da51ea685aff3a4
Ilya1510 <ilya151098@yandex.ru>
2020-03-14 00:13:28 +0300

    commit moving cube

--- tutorial04_colored_cube/tutorial04.cpp
+++ tutorial04_colored_cube/tutorial04.cpp
@@ -70,59 +70,46 @@ int main( void )
 	GLuint vertexPosition_modelspaceID = glGetAttribLocation(programID, "vertexPosition_modelspace");
 	GLuint vertexColorID = glGetAttribLocation(programID, "vertexColor");
 
-	// Projection matrix : 45� Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
-	glm::mat4 Projection = glm::perspective(45.0f, 4.0f / 3.0f, 0.1f, 100.0f);
-	// Camera matrix
-	glm::mat4 View       = glm::lookAt(
-								glm::vec3(4,3,-3), // Camera is at (4,3,-3), in World Space
-								glm::vec3(0,0,0), // and looks at the origin
-								glm::vec3(0,1,0)  // Head is up (set to 0,-1,0 to look upside-down)
-						   );
-	// Model matrix : an identity matrix (model will be at the origin)
-	glm::mat4 Model      = glm::mat4(1.0f);
-	// Our ModelViewProjection : multiplication of our 3 matrices
-	glm::mat4 MVP        = Projection * View * Model; // Remember, matrix multiplication is the other way around
-
 	// Our vertices. Tree consecutive floats give a 3D vertex; Three consecutive vertices give a triangle.
 	// A cube has 6 faces with 2 triangles each, so this makes 6*2=12 triangles, and 12*3 vertices
-	static const GLfloat g_vertex_buffer_data[] = { 
-		-1.0f,-1.0f,-1.0f,
-		-1.0f,-1.0f, 2.0f,
-		-1.0f, 1.0f, 1.0f,
-		 1.0f, 1.0f,-1.0f,
-		-1.0f,-1.0f,-1.0f,
-		-1.0f, 1.0f,-1.0f,
-		 1.0f,-1.0f, 1.0f,
-		-1.0f,-1.0f,-1.0f,
-		 1.0f,-1.0f,-1.0f,
-		 1.0f, 1.0f,-1.0f,
-		 1.0f,-1.0f,-1.0f,
-		-1.0f,-1.0f,-1.0f,
-		-1.0f,-1.0f,-1.0f,
-		-1.0f, 1.0f, 1.0f,
-		-1.0f, 1.0f,-1.0f,
-		 1.0f,-1.0f, 1.0f,
-		-1.0f,-1.0f, 2.0f,
-		-1.0f,-1.0f,-1.0f,
-		-1.0f, 1.0f, 1.0f,
-		-1.0f,-1.0f, 2.0f,
-		 1.0f,-1.0f, 1.0f,
-		 1.0f, 2.0f, 1.0f,
-		 1.0f,-1.0f,-1.0f,
-		 1.0f, 1.0f,-1.0f,
-		 1.0f,-1.0f,-1.0f,
-		 1.0f, 2.0f, 1.0f,
-		 1.0f,-1.0f, 1.0f,
-		 1.0f, 2.0f, 1.0f,
-		 1.0f, 1.0f,-1.0f,
-		-1.0f, 1.0f,-1.0f,
-		 1.0f, 2.0f, 1.0f,
-		-1.0f, 1.0f,-1.0f,
-		-1.0f, 1.0f, 1.0f,
-		 1.0f, 2.0f, 1.0f, //(1,1,1)->(1,2,1)
-		-1.0f, 1.0f, 1.0f,
-		 1.0f,-1.0f, 1.0f
-	};
+    static const GLfloat g_vertex_buffer_data[] = {
+            -1.0f,-1.0f,-1.0f, // Треугольник 1 : начало
+            -1.0f,-1.0f, 1.0f,
+            -1.0f, 2.0f, 2.0f, // Треугольник 1 : конец
+            1.0f, 1.0f,-1.0f, // Треугольник 2 : начало
+            -1.0f,-1.0f,-1.0f,
+            -1.0f, 1.0f,-1.0f, // Треугольник 2 : конец
+            1.0f,-1.0f, 1.0f,
+            -1.0f,-1.0f,-1.0f,
+            1.0f,-1.0f,-1.0f,
+            1.0f, 1.0f,-1.0f,
+            1.0f,-1.0f,-1.0f,
+            -1.0f,-1.0f,-1.0f,
+            -1.0f,-1.0f,-1.0f,
+            -1.0f, 2.0f, 2.0f,
+            -1.0f, 1.0f,-1.0f,
+            1.0f,-1.0f, 1.0f,
+            -1.0f,-1.0f, 1.0f,
+            -1.0f,-1.0f,-1.0f,
+            -1.0f, 2.0f, 2.0f,
+            -1.0f,-1.0f, 1.0f,
+            1.0f,-1.0f, 1.0f,
+            2.0f, 3.0f, 1.0f,
+            1.0f,-1.0f,-1.0f,
+            1.0f, 1.0f,-1.0f,
+            1.0f,-1.0f,-1.0f,
+            2.0f, 3.0f, 1.0f,
+            1.0f,-1.0f, 1.0f,
+            2.0f, 3.0f, 1.0f,
+            1.0f, 1.0f,-1.0f,
+            -1.0f, 1.0f,-1.0f,
+            2.0f, 3.0f, 1.0f,
+            -1.0f, 1.0f,-1.0f,
+            -1.0f, 2.0f, 2.0f,
+            2.0f, 3.0f, 1.0f,
+            -1.0f, 2.0f, 2.0f,
+            1.0f,-1.0f, 1.0f
+    };
 
 	// One color for each vertex. They were generated randomly.
 	static const GLfloat g_color_buffer_data[] = { 
@@ -173,9 +160,14 @@ int main( void )
 	glGenBuffers(1, &colorbuffer);
 	glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
+    float counter = -70;
+    float w = 0.5;
 
-	do{
-
+    do{
+        counter += w;
+        if (counter > 100.0 || counter < -70) {
+            w = -w;
+        }
 		// Clear the screen
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
@@ -184,6 +176,22 @@ int main( void )
 
 		// Send our transformation to the currently bound shader, 
 		// in the "MVP" uniform
+        // Projection matrix : 45� Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
+        glm::mat4 Projection = glm::perspective(45.0f + counter / 100, 4.0f / 3.0f + counter / 1000, 0.1f, 100.0f);
+        // Or, for an ortho camera :
+        //glm::mat4 Projection = glm::ortho(-10.0f,10.0f,-10.0f,10.0f,0.0f,100.0f); // In world coordinates
+
+        // Camera matrix
+        glm::mat4 View       = glm::lookAt(
+                glm::vec3(counter / 4 - 10,counter / 10,counter / 10), // Camera is at (4,3,3), in World Space
+                glm::vec3(0 + (counter / 20),counter / 100,counter / 40), // and looks at the origin
+                glm::vec3(0,1,0)  // Head is up (set to 0,-1,0 to look upside-down)
+        );
+        // Model matrix : an identity matrix (model will be at the origin)
+        glm::mat4 Model      = glm::mat4(1.0f);
+        // Our ModelViewProjection : multiplication of our 3 matrices
+        glm::mat4 MVP        = Projection * View * Model; // Remember, matrix multiplication is the other way around
+
 		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
 
 		// 1rst attribute buffer : vertices
